bb0c52776759d76bbce4f9322b8842f317bd840a
Omar El Hefny <o.hefny@gmail.com>
2018-05-02 01:42:47 +0200

    changes to db to remote db

--- Config/index.js
+++ Config/index.js
@@ -1,6 +1,8 @@
-var db_config=require('./db_config');
+//var db_config=require('./db_config');
 module.exports={
     getDBConnectionStr:function () {
-        return db_config.url+'/'+db_config.databaseName;
+  //      var str= db_config.key+"//"+db_config.credentials+"@"+db_config.url+"@"+db_config.port+'/'+db_config.databaseName;
+    //    console.log(str);
+        return "mongodb://dorgham:dorgham@ds111430.mlab.com:11430/banksystemdorgham";
     }
 }
